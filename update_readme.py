7ee3b92a23c75d2549c60c0955f4deda7aaeefec
seungsoon Kim <74717033+Kimgabe@users.noreply.github.com>
2024-01-20 12:25:08 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -3,51 +3,55 @@ import re
 
 # GitHub API를 통해 특정 저장소의 폴더 목록을 가져오는 함수
 def get_folders(repo):
+    # GitHub API를 사용하기 위한 URL 생성
     url = f"https://api.github.com/repos/{repo}/contents/"
     response = requests.get(url)
-    # '.'으로 시작하는 폴더는 제외
+    # API로부터 받은 응답을 JSON 형식으로 파싱하여 저장
     folders = [item for item in response.json() if item['type'] == 'dir' and not item['name'].startswith('.')]
     return folders
 
 # 폴더 이름에서 숫자를 추출하는 함수
 def extract_number(folder_name):
+    # 정규 표현식을 사용하여 폴더 이름에서 숫자를 추출
     match = re.search(r'\d+', folder_name)
     if match:
         return int(match.group())
-    return float('inf')
+    return float('inf')  # 숫자가 없는 경우 무한대 값을 반환
 
 # README.md 파일을 업데이트하는 함수
 def update_readme(repo, folders, original_content):
-    # "## 📑Quest List📑" 섹션과 그 이하 내용 제거
+    # 기존 "## 📑Quest List📑" 섹션과 그 이하 내용을 제거
     start_index = original_content.find("## 📑Quest List📑")
     if start_index != -1:
         original_content = original_content[:start_index]
 
-    # 새로운 "## 📑Quest List📑" 테이블 생성
+    # 새로운 "## 📑Quest List📑" 섹션 및 테이블을 생성
     new_table = "## 📑Quest List📑\n\n"
     new_table += "| 퀘스트명 | URL |\n"
     new_table += "| --- | --- |\n"
 
+    # 폴더를 숫자로 정렬하여 목록을 만듭니다.
     sorted_folders = sorted(folders, key=lambda x: extract_number(x['name']))
     for folder in sorted_folders:
         folder_name = folder['name']
         folder_url = folder['html_url']
         new_table += f"| {folder_name} | [Link]({folder_url}) |\n"
 
-    # README.md 파일의 기존 내용 끝에 새로운 테이블 추가
+    # README.md 파일의 기존 내용 끝에 새로운 테이블을 추가
     updated_content = original_content + new_table
     return updated_content
 
 # GitHub 저장소와 폴더 목록을 설정
 repo = "Kimgabe/AIFFEL_Online_Quest"
 
-# 기존 README.md 내용을 읽기
+# 기존 README.md 내용을 읽어옵니다.
 with open("README.md", "r") as file:
     original_content = file.read()
 
+# 저장소에서 폴더 목록을 가져오고 README.md 파일을 업데이트
 folders = get_folders(repo)
 updated_content = update_readme(repo, folders, original_content)
 
-# README.md 파일에 쓰기
+# 업데이트된 내용을 README.md 파일에 업데이트
 with open("README.md", "w") as file:
     file.write(updated_content)
